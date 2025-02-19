ce1c8de9ffd11fd75ce545f490830d745964549a
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-19 15:44:29 +0530

    Add error handler

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongoDB.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 console.log('Port:', PORT);
 
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use(errorMiddleware);
 app.get('/', (req, res) => {
     res.send('Welcome to Subscription Tracker!');
 });
