5fbb75a8ff7c904e7234f24db998998437a18191
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-19 19:54:28 +0530

    add Authentication

--- config/env.js
+++ config/env.js
@@ -8,4 +8,10 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 console.log("NODE_ENV:", process.env.NODE_ENV);  // Check the current environment
 console.log("PORT:", process.env.PORT);  // Check if PORT is loaded correctly
 
-export const { PORT, NODE_ENV ,DB_URI} = process.env;
+export const { 
+    PORT, 
+    NODE_ENV ,
+    DB_URI,
+    JWT_SECRET,
+    JWT_EXPIRES_IN
+    } = process.env;
