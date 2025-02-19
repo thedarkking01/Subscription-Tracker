391276a62de0659b1a8d97fd2f6855a84f2fd3a4
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-20 00:31:44 +0530

    Add ARCJET

--- app.js
+++ app.js
@@ -6,18 +6,21 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongoDB.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 console.log('Port:', PORT);
 
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 app.use(errorMiddleware);
+
 app.get('/', (req, res) => {
     res.send('Welcome to Subscription Tracker!');
 });
--- config/env.js
+++ config/env.js
@@ -13,5 +13,7 @@ export const {
     NODE_ENV ,
     DB_URI,
     JWT_SECRET,
-    JWT_EXPIRES_IN
+    JWT_EXPIRES_IN,
+    ARCJET_KEY,
+    ARCJET_ENV
     } = process.env;
