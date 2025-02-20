3b0ce4cfa830b43326c3f881f940bdb4e4c63c47
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-02-20 19:25:15 +0530

    Add workflow Uptash

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongoDB.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 
 const app = express();
@@ -19,6 +20,7 @@ console.log('Port:', PORT);
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
 app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
